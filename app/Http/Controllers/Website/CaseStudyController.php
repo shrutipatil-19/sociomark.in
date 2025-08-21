c6b8df0d03e482e219f9c8d2b1395af49c7fe969
shrutipatil-19 <shrupatil1319@gmail.com>
2025-08-21 12:33:53 +0530

    case study meta title desc

--- app/Http/Controllers/Website/CaseStudyController.php
+++ app/Http/Controllers/Website/CaseStudyController.php
@@ -19,129 +19,126 @@ class CaseStudyController extends Controller
     public function stemrxbio()
     {
         $meta = [
-            'title' => 'Stemrx Case study| Sociomark UAE​',
-            'description' => "Explore how our digital marketing strategies helped StemRx enhance its online presence through SEO, social media, content creation, and targeted campaigns.",
-            'keywords' => 'STEMRxBio marketing, healthcare digital marketing, pharma social media, Best digital marketing agency in Dubai, Best digital marketing agency to work with in Ajman, Digital marketing agency in UAE'
+            'title' => 'StemRx Bioscience | Sociomark Case Study​',
+            'description' => "Detailed case study of StemRx Bioscience. Sociomark performs a 360-degree digital marketing plan for them including SEO, SMM, SEM, Web Development etc.",
+            'keywords' => 'case study, client case study, client work'
         ];
         return view('Frontend/caseStudy/stemrxbio', compact('meta'));
     }
     public function expert4travel()
     {
         $meta = [
-            'title' => 'Expert4Travel Case Study | Sociomark UAE​',
-            'description' => "Discover how our digital marketing strategies helped Expert4Travel boost its online visibility, drive bookings with targeted campaigns.",
-            'keywords' => 'Expert4Travel travel marketing, tourism digital campaigns, travel SEO, Best digital marketing agency in Dubai, Best digital marketing agency to work with in Ajman, Digital marketing agency in UAE'
+            'title' => 'expert4travel | Sociomark Case Study​',
+            'description' => "Detailed case study of expert4travel. Sociomark performs a 360-degree digital marketing plan for them including SEO, SMM, SEM, Web Development etc.",
+            'keywords' => 'case study, client case study, client work'
         ];
         return view('Frontend/caseStudy/expert4travel', compact('meta'));
     }
     public function theProfessionalCouriers()
     {
         $meta = [
-            'title' => 'The Professional Couriers Case Study by Sociomark​​',
-            'description' => "Discover how Sociomark, a Digital marketing agency, boosted The Professional Couriers’ brand visibility and engagement through smart social media strategies.",
-            'keywords' => 'courier services marketing, logistics digital campaigns, transportation SEO, Best digital marketing agency in Dubai, Best digital marketing agency to work with in Ajman, Digital marketing agency in UAE'
+            'title' => 'The Professional Couriers | Sociomark Case Study​',
+            'description' => "Detailed case study of The Professional Couriers. Sociomark performs a 360-degree digital marketing plan including SMM, SEM, ORM & Online Advertising.",
+            'keywords' => 'the professional couriers, TPC, Case Study, client case study, client work, best digital marketing agency in Navi Mumbai'
         ];
         return view('Frontend/caseStudy/theProfessionalCouriers', compact('meta'));
     }
     public function eduauraa()
     {
         $meta = [
-            'title' => 'Eduauraa Case Study | Sociomark Digital Agency UAE​​',
-            'description' => "Discover how Sociomark, a digital marketing agency in UAE, helped Eduauraa boost its online presence through smart strategies and impactful digital campaigns.",
-            'keywords' => 'EduAuraa education marketing, school digital strategy, edtech campaigns,Best digital marketing agency in Dubai, Best digital marketing agency to work with in Ajman, Digital marketing agency in UAE'
+            'title' => 'Eduauraa | Sociomark Case Study​​',
+            'description' => "Detailed case study of Eduauraa. Sociomark performs a 360-degree digital marketing plan for them including SEO, SMM, SEM, Web Development etc.",
+            'keywords' => 'case study, client case study, client work'
         ];
         return view('Frontend/caseStudy/eduauraa', compact('meta'));
     }
     public function shahenaz()
     {
         $meta = [
-            'title' => 'Shahenaz Case Study | Sociomark Success Story​',
-            'description' => "Discover how our digital marketing agency in UAE helped Shahenaz boost its brand online through smart strategy, creativity, and impactful campaigns.",
-            'keywords' => 'fashion marketing, clothing brand digital strategy, apparel social media, fashion brand SEO, ethnic wear marketing, lifestyle brand promotions'
+            'title' => "Driving Growth: Sociomark's Digital Marketing for Shahenaz.​",
+            'description' => "Witness the power of Sociomark's comprehensive digital marketing strategy for Shahenaz, encompassing SEO, SMM,and beyond.",
+            'keywords' => 'case study, client case study, client work'
         ];
         return view('Frontend/caseStudy/shahenaz', compact('meta'));
     }
     public function okinawascooters()
     {
         $meta = [
-            'title' => 'Okinawa Scooters Case Study | Sociomark UAE​​',
-            'description' => "Discover how our digital marketing agency in UAE helped Okinawa Scooters grow online through creative social media and strategic digital campaigns.",
-            'keywords' => 'Okinawa scooters marketing, automotive digital campaigns, scooter SEO, Best digital marketing agency in Dubai, Best digital marketing agency to work with in Ajman, Digital marketing agency in UAE'
+            'title' => 'OKINAWA | Sociomark Case Study​',
+            'description' => "Detailed case study of OKINAWA. Sociomark performs a 360-degree digital marketing plan for them including SEO, SMM, SEM, Web Development etc.",
+            'keywords' => 'case study, client case study, client work'
         ];
         return view('Frontend/caseStudy/okinawascooters', compact('meta'));
     }
     public function ayushakti()
     {
         $meta = [
-            'title' => 'Ayushakti Case Study | Sociomark Dubai ​​',
-            'description' => "See how our digital marketing efforts helped Ayushakti expand its reach, strengthen brand presence, through strategic content, SEO, and social media.",
-            'keywords' => 'Ayushakti wellness marketing, healthcare digital, Ayurveda social media, Best digital marketing agency in Dubai, Best digital marketing agency to work with in Ajman, Digital marketing agency in UAE'
+            'title' => 'Ayushakti Ayurved | Sociomark Case Study​​',
+            'description' => "Detailed case study of Ayushakti Ayurved. Sociomark performs a 360-degree digital marketing plan for them including SEO, SMM, SEM, Web Development etc.",
+            'keywords' => 'case study, client case study, client work'
         ];
         return view('Frontend/caseStudy/ayushakti', compact('meta'));
     }
     public function lime()
     {
         $meta = [
-            'title' => 'Lessons in Marketing Excellence (LIME)- Sociomark Case Study​​',
-            'description' => "Sociomark in collabration with other content creators worked on 
-LIME Season XII by promoting the hastags for this year which was #marketingisbusiness.",
-            'keywords' => 'LIME case study, brand strategy initiatives, corporate marketing education,Best digital marketing agency in Dubai, Best digital marketing agency to work with in Ajman, Digital marketing agency in UAE '
+            'title' => 'Lessons in Marketing Excellence (LIME)- Sociomark Case Study​',
+            'description' => "Sociomark in collabration with other content creators worked on LIME Season XII by promoting the hastags for this year which was #marketingisbusiness.",
+            'keywords' => 'case study, client case study, client work, Lessons in Marketing Excellence, HUL, CNBC TV18, B-School Competition'
         ];
         return view('Frontend/caseStudy/lime', compact('meta'));
     }
     public function nodwinGaming()
     {
         $meta = [
-            'title' => 'Nodwin Gaming Project | Sociomark  Work​​',
-            'description' => "See how our digital marketing agency in UAE helped Nodwin Gaming boost their brand visibility with creative social media and digital strategies.",
-            'keywords' => 'Nodwin Gaming esports marketing, gaming digital campaigns, esports SEO, Best digital marketing agency in Dubai, Best digital marketing agency to work with in Ajman, Digital marketing agency in UAE'
+            'title' => 'Gaming Industry - Nodwin Gaming and Social Media - Case Study​​',
+            'description' => "Social Media strategy for Nodwin gaming, to increase & strengthen its digital presence in the exponentially booming ecosystem of gaming & esports.",
+            'keywords' => 'nodwin gaming, social media case study, gaming industry case study, digital markeitng case study'
         ];
         return view('Frontend/caseStudy/nodwinGaming', compact('meta'));
     }
     public function buttondabaodeshbanao()
     {
         $meta = [
-            'title' => 'Button Dabao Desh Banao Campaign by Sociomark​',
-            'description' => "Sociomark, a Digital marketing agency, created an engaging social media campaign for Button Dabao Desh Banao to boost brand reach and audience interaction.",
-            'keywords' => ' public service digital campaigns, election awareness strategy, Button Dabao Desh Banao, social media for voting rights, government awareness campaign UAE'
+            'title' => 'Case Study | Button Dabao Desh Banao | Lok Sabha Elections Campaign​',
+            'description' => "Detailed analysis of 'BDDB' campaign strategized by Sociomark for Network18 to encourage Indian voters to cast their vote in 2019 Lok Sabha Elections.",
+            'keywords' => 'Case Study, Button Dabao Desh Banao'
         ];
         return view('Frontend/caseStudy/buttondabaodeshbanao', compact('meta'));
     }
     public function roadtosafetyinitiative()
     {
         $meta = [
-            'title' => 'Road to Safety Initiative by Sociomark Agency​​',
-            'description' => "Sociomark, a Digital marketing agency, led the Road to Safety Initiative to create awareness through impactful social media strategies and engagement.",
-            'keywords' => 'road safety marketing, public sector digital campaigns, CSR SEO, Best digital marketing agency in Dubai, Best digital marketing agency to work with in Ajman, Digital marketing agency in UAE'
+            'title' => 'Case Study | Road To Safety Initiative | Diageo - Network18 Campaign​',
+            'description' => "Detailed analysis of 'Road To Safety' campaign strategized by Sociomark for Diageo & Network18 to encourage Indians to drive responsibly & be safe on roads.",
+            'keywords' => 'Case Study, Road To Safety Initiative, Road To Safety'
         ];
         return view('Frontend/caseStudy/roadtosafetyinitiative', compact('meta'));
     }
     public function acetattoz()
     {
         $meta = [
-            'title' => 'Acetattoz Case Study by Sociomark Digital Experts​​',
-            'description' => "Explore how Sociomark, a Digital marketing agency, helped Acetattoz increase brand awareness and engagement through targeted social media strategies.",
-            'keywords' => 'tattoo studio marketing, Ace Tattoos branding, body art promotion UAE, ink studio social media campaigns, youth lifestyle branding, tattoo artist digital strategy, Best digital marketing agency in Dubai, Best digital marketing agency to work with in Ajman, Digital marketing agency in UAE'
+            'title' => 'Acetattoz | Sociomark Case Study​​',
+            'description' => "Detailed case study of Ayushakti Ayurved. Sociomark performs a 360-degree digital marketing plan for them including SEO, SMM, SEM, Web Development etc.",
+            'keywords' => 'case study, client case study, client work'
         ];
         return view('Frontend/caseStudy/acetattoz', compact('meta'));
     }
     public function smarterIndia()
     {
         $meta = [
-            'title' => 'Smarter India Campaign Case Study | Powering Awareness Digitally',
-            'description' => "A deep dive into how we executed the impactful “Button Dabao, Desh Banao” campaign driving awareness, through powerful digital storytelling and strategy.",
-            'keywords' => 'Smarter India consulting marketing, education digital, corporate SEO,Best digital marketing agency in Dubai, Best digital marketing agency to work with in Ajman, Digital marketing agency in UAE'
+            'title' => 'Lenovo Smarter India- Sociomark Case Study',
+            'description' => "Transforming the world intelligently is the vision behind 'Smarter India'. Sociomark amplified this perspective on LinkedIn with some professional tactics",
+            'keywords' => 'Lenovo, Smarter India, Linkedin Marketing, Social Media, Case Study, Linkedin Optimization, Linkedin Digital Marketing'
         ];
         return view('Frontend/caseStudy/smarterIndia', compact('meta'));
     }
     public function realestateawards()
     {
         $meta = [
-            'title' => 'RR Kabel Case Study | Boosting Brand Awareness in Electricals​​',
-            'description' => "Explore how RR Kabel strengthened its digital presence and
- drove customer engagement through effective marketing strategies
- by Sociomark .",
-            'keywords' => 'real estate awards campaign, property marketing, realty digital, Best digital marketing agency in Dubai, Best digital marketing agency to work with in Ajman, Digital marketing agency in UAE'
+            'title' => 'Case Study | RR Kabel Presents CNBC AWAAZ Real Estate Awards​​',
+            'description' => "Here's a detailed case study of the social media marketing campaign strategized by the Sociomark Digital Marketing Agency for RR Kabel & CNBC Awaaz Real Estate Awards.",
+            'keywords' => 'Case Study, Real Estate Awards, Social Media Campaigns, Successful Marketing Campaigns, Marketing Campaigns'
         ];
         return view('Frontend/caseStudy/realestateawards', compact('meta'));
     }
