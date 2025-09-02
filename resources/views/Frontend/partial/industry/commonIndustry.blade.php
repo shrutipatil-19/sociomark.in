2297cf85a77d464ea0e6d1a28fa4635389324476
Kunal Mahajan <kunalmahajan976757@gmail.com>
2025-09-02 19:15:34 +0530

    common industry change

--- resources/views/Frontend/partial/industry/commonIndustry.blade.php
+++ resources/views/Frontend/partial/industry/commonIndustry.blade.php
@@ -27,11 +27,11 @@
         </div>
 
         <div class="portfolio-area" data-aos="zoom-in" data-aos-delay="200">
-            <div class="row gy-4 filter-active garally-row-height mt-md-0 mt-4">
+            <div class="row">
                 @foreach ($clients as $slug => $client)
                     @if(!empty($client['image']))
-                        <div class="col-md-3 col-6 col-xl-auto filter-item cat4 cat6">
-                            <a href="{{ route('social_media', $slug) }}" target="_blank" class="project-item d-block">
+                        <div class="col-md-3 col-6 col-xl-auto">
+                            <a href="{{ route('social_media', $slug) }}" target="_blank" class="project-item mb-3 d-block">
                                 <img src="{{ asset($client['image']) }}" alt="{{ $slug }} project image" class="w-100">
                             </a>
                         </div>
