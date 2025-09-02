aeeaf79cf8964fb95a3884d69d266e7a91980a9f
Kunal Mahajan <kunalmahajan976757@gmail.com>
2025-09-02 10:48:04 +0530

    worl culture chnge

--- resources/views/Frontend/WorkCulture.blade.php
+++ resources/views/Frontend/WorkCulture.blade.php
@@ -233,7 +233,7 @@
             </picture>
         </div>
     </section>
-    <section class="overflow-hidden space">
+    <section class="overflow-hidden space d-none">
         <div class="container">
             <div class="row">
                 <div class="col-12">
@@ -249,7 +249,7 @@
             </div>
         </div>
     </section>
-    <section class="about-area2 overflow-hidden pb-md-5 pb-3 pt-2" id="about-sec">
+    <section class="about-area2 overflow-hidden space pt-5" id="about-sec">
         <div class="container">
             <div class="row flex-row-reverse align-items-start">
                 <div class="col-lg-6 mb-30 mb-xl-0">
